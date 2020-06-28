2925e821bfbe8193592929d6049a3613d91e0757
dinek <wilk.dinek@gmail.com>
2020-06-29 00:58:23 +0200

    Updates

---
+++ precache-manifest.75893d13804cb335caa7c6cb1c5def33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50cd0dbaf187060f50c2ff4893bfa3e",
+    "url": "/Poker/index.html"
+  },
+  {
+    "revision": "c98af4ef9d571064afb5",
+    "url": "/Poker/static/js/2.ac95eb19.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/Poker/static/js/2.ac95eb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee9e448d1aa7b6ce23d",
+    "url": "/Poker/static/js/main.cfbb2e8d.chunk.js"
+  },
+  {
+    "revision": "6a7ee455f843fdae47b3",
+    "url": "/Poker/static/js/runtime-main.9934cff1.js"
+  }
+]);
